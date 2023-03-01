0955c0db3215d36798ff16342927d5c3becaea86
Aiman1080 <114600986+Aiman1080@users.noreply.github.com>
2023-03-01 20:10:34 +0100

    commit les 3

--- SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
+++ SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfCompare
 {
@@ -35,7 +27,6 @@ namespace WpfCompare
 
             string[] files = Directory.GetFiles(startfolder1, "*.txt");
 
-
             foreach (string fileName in files)
             {
                 lbxBestanden1.Items.Add(System.IO.Path.GetFileName(fileName));
@@ -54,14 +45,67 @@ namespace WpfCompare
             }
         }
 
+        private void Weergave1(string filePath)
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            lbxLines1.ItemsSource = lines;
+        }
+        private void Weergave2(string filePath)
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            lbxLines2.ItemsSource = lines;
+        }
+
         private void lbxBestanden1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string filePath = System.IO.Path.Combine(folderPath, "myfile.txt");
+            string selectedFile = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "test1", (string)lbxBestanden1.SelectedItem);
+            Weergave1(selectedFile);
+        }
 
-            string[] regels = File.ReadAllLines(filePath);
+        private void lbxBestanden2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedFile = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "test2", (string)lbxBestanden2.SelectedItem);
+            Weergave2(selectedFile);
+        }
 
-            
+        private void btnCompare_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < lbxLines1.Items.Count && i < lbxLines2.Items.Count; i++)
+            {
+                ListBoxItem item1 = lbxLines1.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
+                ListBoxItem item2 = lbxLines2.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
+
+                if (item1 != null && item2 != null)
+                {
+                    string[] words1 = item1.Content.ToString().Split(' ');
+                    string[] words2 = item2.Content.ToString().Split(' ');
+
+                    for (int j = 0; j < words1.Length && j < words2.Length; j++)
+                    {
+                        if (words1[j] != words2[j])
+                        {
+                            item1.Background = Brushes.Red;
+                            item2.Background = Brushes.Red;
+                        }
+                    }
+                }
+            }
         }
     }
 }
