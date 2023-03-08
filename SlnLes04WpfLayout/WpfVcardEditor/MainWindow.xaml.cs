c8b2c347f7ceee5354535e265a84865a53dcfcf4
Aiman1080 <114600986+Aiman1080@users.noreply.github.com>
2023-03-08 21:13:56 +0100

    commit les 4

---
+++ SlnLes04WpfLayout/WpfVcardEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfVcardEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clkAbout_Click(object sender, RoutedEventArgs e)
+        {
+            new AboutVenster().Show();
+        }
+
+        private void clkExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult msg = MessageBox.Show("ben je zeker dat je de applicatie wil afsluiten?", "Toepassing sluiten", MessageBoxButton.OKCancel);
+
+            if (msg == MessageBoxResult.OK) 
+            {
+                Application.Current.MainWindow.Close();
+            }
+
+        }
+
+    }
+}
